bb402940ba781baedfac1cf3f8a586d8a37c1f85
AvetisDN <avetisdn@gmail.com>
2019-01-24 10:45:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+(function () {
+    // side menu
+    let burger = document.querySelector('#burger-menu');
+    let sideMenu = document.querySelector('.main-menu--list');
+    burger.addEventListener('click', toggleSideMenu);
+
+    function toggleSideMenu(e) {
+        e.preventDefault();
+        sideMenu.classList.toggle('open');
+    }
+
+    $(function () {
+        if ("ontouchstart" in window) {
+            $(document).swipe({
+                swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+                    
+                    if (direction == "left") {
+                        $(sideMenu).addClass('open');
+                    }
+                    if (direction == "right") {
+                        $(sideMenu).removeClass('open');
+                    }
+                },
+                threshold: 0
+            });
+        }
+    });
+
+})();
